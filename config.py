6a29a9172d9cbe338f8d4767eb65dc802cc1f3e3
Poutrashkov-afk <poutrashkov@gmail.com>
2026-02-28 09:11:15 +0100

    added the mistral api key to config

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 """
 Config module for storing api keys, model names, and other configuration variables.
 """
+import os
+from dotenv import load_dotenv
+load_dotenv()
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+
