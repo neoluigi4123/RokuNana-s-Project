f83af47cd09cb8686b6177531ad4c6bf4d8cb5cb
Poutrashkov-afk <poutrashkov@gmail.com>
2026-02-28 09:03:27 +0100

    added config, core and main.py as well as requirements

---
+++ main.py
@@ -0,0 +1,3 @@
+"""
+Main module for the RokuNana's Project. This is where the main execution of the program will happen. It will import necessary modules and run the main loop for the llm agent.
+"""
