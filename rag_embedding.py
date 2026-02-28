5c5ad0dc6348dc28bcdbd10ffbb4c58a8851b77a
neoluigi4123 <neoluigimario2008@gmail.com>
2026-02-28 11:38:15 +0100

    fixed rag module

--- rag_embedding.py
+++ rag_embedding.py
@@ -13,15 +13,15 @@ def read_memory(n, query=""):
     with open("data.csv", newline="", encoding="utf-8") as f:
         docs = [f"{r['content']}" for r in csv.DictReader(f)]
 
-    embeds_response = client.embeddings.create(model=MODEL, input=docs)
-    embeds = [item.embedding for item in embeds_response.data]
+    embeds_response = client.embeddings.create(model=MODEL, inputs=docs)
+    embeds =[item.embedding for item in embeds_response.data]
     DB.upsert(ids=[str(i) for i in range(len(docs))], embeddings=embeds, documents=docs)
 
-    q_embed_response = client.embeddings.create(model=MODEL, input=query)
+    q_embed_response = client.embeddings.create(model=MODEL, inputs=[query])
     q_embed = q_embed_response.data[0].embedding
     res = DB.query(query_embeddings=[q_embed], n_results=n)
     
-    return res['documents'][0] if res['documents'] else []
+    return res['documents'][0] if res['documents'] else[]
 
 def write_memory(content):
     with open("data.csv", "a", newline="", encoding="utf-8") as f:
