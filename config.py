a499988f507c4d0daaf70eb9f9fabb56e5484fd4
Poutrashkov-afk <poutrashkov@gmail.com>
2026-02-28 09:30:01 +0100

    added a discord bot token

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 DEFAULT_MODEL = "mistral-large-latest"
 DEFAULT_TTS_MODEL = "voxtral-realtime-latest"
